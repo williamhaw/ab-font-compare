c23a395937246205228a652a0baa2c099a067cf8
williamhaw <me@williamhaw.com>
2019-04-17 01:28:35 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <Piece content="Welcome to Your Vue.js App" font="Oswald" />
+  </div>
+</template>
+
+<script>
+import Piece from "./components/Piece.vue";
+
+var WebFont = require("webfontloader");
+
+WebFont.load({
+  google: {
+    families: ["Oswald"]
+  }
+});
+
+export default {
+  name: "app",
+  components: {
+    Piece
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Piece.vue
@@ -0,0 +1,24 @@
+<template>
+  <textarea
+    v-bind:style="{ fontFamily: font }"
+    v-model="content"
+    class="piece"
+  />
+</template>
+
+<script>
+export default {
+  name: "Piece",
+  props: {
+    content: String,
+    font: String
+  }
+};
+</script>
+
+<style>
+.piece {
+  border-style: hidden;
+  resize: none;
+}
+</style>
